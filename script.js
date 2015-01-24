d388a85d295d20d7e34c9b5095de7b4054f5beeb
Noah Muth <noahmuth@gmail.com>
2015-01-24 10:59:04 -0600

    Added based IDs.

--- script.js
+++ script.js
@@ -4,9 +4,31 @@ navigator.getUserMedia  = navigator.getUserMedia
     || navigator.msGetUserMedia;
 
 function getPeerId() {
-  var words = [
-    'correct', 'horse', 'battery', 'staple', 'based', 'god',
-  ]
+  var basedWords = [
+  	"rare", "based", "TYBG", "fuck your bitch", "fuck my bitch",
+  	"finna", "hit a lick", "tryna", "bust a nut", "what are you doing",
+  	"Brandon", "BasedWorld", "trapped", "Keke", "Lil B", "BasedGod",
+  	"family", "basedfam", "fakebased", "I'mma", "fuck me", "ho",
+  	"4 me", "bricks", "20 bricks", "smoke", "heata", "burna", "comin through",
+  	"with the", "murder rate", "pink flame", "green flame", "red flame",
+  	"white flame", "black flame", "100% percent gutta", "god's father",
+  	"mixtape", "track", "trap", "real", "realest", "this the realest",
+  	"rawest", "rapper", "alive", "I love you", "don't feel pressured to have sex",
+  	"flex 36es", "Paris Hilton", "Ellen Degeneres", "JK Rowling", "I look like",
+  	"I feel like", "ya fill me", "I go dumb", "go so dumb", "dumb", "niggas",
+  	"my niggas", "fake", "ass", "eat her", "pussy", "suck my dick", "on my dick",
+  	"hipster girls", "underground party", "New York", "Berkeley", "comin from da under",
+  	"Katy Perry", "Kevin Durant", "fuck", "spit", "bitches", "clink clink",
+  	"I wanna", "say something", "too many tags", "the damn beat", "hoop life",
+  	"streets", "life's hard", "ratchet", "I need", "I need some", "shout out",
+  	"someone stole my sunglasses", "parents", "thank", "happy", "you're"
+  ];
+  
+  var words = basedWords[Math.floor(Math.random() * basedWords.length)]
+    + ' ' + basedWords[Math.floor(Math.random() * basedWords.length)]
+    + ' ' + basedWords[Math.floor(Math.random() * basedWords.length)];
+  
+  return words;
 }
 
 function audioAPI() {
@@ -25,7 +47,7 @@ function audioAPI() {
   });
 }
 
-var peer = new Peer({
+var peer = new Peer(getPeerId(), {
   key: 'l10zoxgcc0s8m2t9'
 });
 
