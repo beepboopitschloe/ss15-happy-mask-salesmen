79a31f4b3a89c9ea2f41abfafe8eec0f5ba207c3
Noah Muth <noahmuth@gmail.com>
2015-01-25 13:33:13 -0600

    Pulled out based words.

--- src/views/room.js
+++ src/views/room.js
@@ -53,59 +53,6 @@ function createCallerWidget(stream, metadata) {
   $('.loader').remove();
 }
 
-function getPeerId() {
-  var basedWords = [
-    "rare", "based", "TYBG", "fuck your bitch", "fuck my bitch",
-    "finna", "hit a lick", "tryna", "bust a nut", "what are you doing",
-    "Brandon", "BasedWorld", "trapped", "Keke", "Lil B", "BasedGod",
-    "family", "basedfam", "fakebased", "Imma", "fuck me", "ho",
-    "4 me", "bricks", "20 bricks", "smoke", "heata", "burna", "comin through",
-    "with the", "murder rate", "pink flame", "green flame", "red flame",
-    "white flame", "black flame", "100% percent gutta", "gods father",
-    "mixtape", "track", "trap", "real", "realest", "this the realest",
-    "rawest", "rapper", "alive", "I love you", "dont feel pressured to have sex",
-    "flex 36es", "Paris Hilton", "Ellen Degeneres", "JK Rowling", "I look like",
-    "I feel like", "ya fill me", "I go dumb", "go so dumb", "dumb",
-    "fake", "ass", "eat her", "pussy", "suck my dick", "on my dick",
-    "hipster girls", "underground party", "New York", "Berkeley", "comin from da under",
-    "Katy Perry", "Kevin Durant", "fuck", "spit", "bitches", "clink clink",
-    "I wanna", "say something", "too many tags", "the damn beat", "hoop life",
-    "streets", "lifes hard", "ratchet", "I need", "I need some", "shout out",
-    "someone stole my sunglasses", "parents", "thank", "happy", "youre",
-    "thugged out pissed off", "child support me", "figaro", "figara",
-    "mama", "smoke on mary", "wonton soup", "thats just how I do",
-    "no black person is ugly", "05 fuck em", "hadouken", "florida", "texas",
-    "futbol", "i own swag", "motherfucker", "BasedLayer", "welcome",
-    "task force", "soldier", "lieutenant", "general", "5150", "breathe slow",
-    "spongebob squarepants", "strapped with the heater", "spongebob BasedGod",
-    "cocaine in my fanny pack", "my nana is strapped", "brodey comin up",
-    "ski ski", "ski mask thug", "ski mask thuggin", "twerk sum",
-    "stop pausing my damn beat", "don't pause the beat", "i know youre a freak",
-    "I met the BasedGod", "I know Lil B", "for Lil Boss", "more relaxed",
-    "better for me", "bitch mob", "4 my", "sorry Rihanna", "sorry Flocker",
-    "Riff Raff", "Joey Badass", "The Game", "is irrelevant", "beef",
-    "call me", "John Stamos", "Kurt Angle", "Dr Phil", "cute", "funny",
-    "random", "must collect", "very straightforward", "unreleased", "secret",
-    "NBA", "gotta make the", "1 on 1", "really fun", "WNBA", "fine ass girls",
-    "fuck in the mouth", "booty milk", "booty cheese", "sniff the", "girls",
-    "booty", "panties", "dirty", "feet", "2 rich to pimp", "rent due",
-    "NYU lecture", "MIT lecture", "rick ross", "dont go outside", "death of rap",
-    "I cook", "birth of rap", "giving up", "februarys confessions", "dopeman hunter",
-    "connected in jail", "illusions of grandeur", "Charlie Sheen", "ps2 game collection",
-    "dbz budokai", "whodie", "Andy Milonakis", "Keith Sweat", "cash on deck", "stick",
-    "hit da 4l0", "Miley Cyrus", "I got mo", "Im god", "Jerry Rice", "glassface",
-    "flowers exodus", "dior paint", "the age of information", "california boy",
-    "a zilli", "Caillou", "Bill Bellamy", "Hugh Hefner", "Obama BasedGod",
-    "pretty", "rich", "young", "thug", "robbin and shoppin"
-  ];
-  
-  var words = basedWords[Math.floor(Math.random() * basedWords.length)]
-    + ' ' + basedWords[Math.floor(Math.random() * basedWords.length)]
-    + ' ' + basedWords[Math.floor(Math.random() * basedWords.length)];
-  
-  return words;
-}
-
 function makeCall(peer, id, stream, options) {
   options = options || {};
   
@@ -186,17 +133,22 @@ function getCallAsClient(call, stream) {
 }
 
 function createPeer(stream, id, options) {
-  id = id || getPeerId();
-  
   options = options || {};
   
   var errorHandler = options.onError || function(err) {
     console.error(err);
   };
   
-  var peer = new Peer(id, {
-    key: 'l10zoxgcc0s8m2t9'
-  });
+  var peer;
+  if (id) {
+    peer = new Peer(id, {
+      key: 'l10zoxgcc0s8m2t9'
+    });
+  } else {
+    peer = new Peer({
+      key: 'l10zoxgcc0s8m2t9'
+    });
+  }
 
   peer.on('open', function(id) {
     console.log('peer ID:', id);
