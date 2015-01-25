54b205c06dc12bfe3f5030d40ca547a1de70878d
janekagan <janekagan@live.com>
2015-01-25 16:13:18 -0600

    Chat name updates to hostid

--- src/views/room.js
+++ src/views/room.js
@@ -275,6 +275,8 @@ function createPeer(stream, id, options) {
         mediaConnections:[],
         dataConnections:[]
       };
+      
+      $('#chat-name').html(hostId);
 
       // get stream
       navigator.getUserMedia({
